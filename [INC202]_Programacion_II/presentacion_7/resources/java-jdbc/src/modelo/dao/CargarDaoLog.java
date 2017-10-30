8083cf24be49345867c24f55c0808f4869ce733a
Eduardo Godoy <eduardo.gl@gmail.com>
2017-10-30 16:33:03 -0300

    mas patrones

---
+++ [INC202]_Programacion_II/presentacion_7/resources/java-jdbc/src/modelo/dao/CargarDaoLog.java
@@ -0,0 +1,44 @@
+package modelo.dao;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class CargarDao {
+
+	public void cargarDatos() {
+		File archivo = null;
+		FileReader fileReader = null;
+		BufferedReader br = null;
+		String linea=null;
+		String palabra=null;		
+		try {
+			archivo = new File("dataset-chilebank.csv");
+			fileReader = new FileReader(archivo);
+			br = new BufferedReader(fileReader);
+			br.readLine();// elimino cabecera
+			while ((linea = br.readLine()) != null) {
+				// System.out.println(linea);
+		        int numTokens = 0;
+		        StringTokenizer st = new StringTokenizer (linea,";");	           
+		        while (st.hasMoreTokens()) {
+		        	palabra = st.nextToken();
+		            numTokens++;
+		            System.out.println ("    Palabra " + numTokens + " es: " + palabra);
+		        }
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
