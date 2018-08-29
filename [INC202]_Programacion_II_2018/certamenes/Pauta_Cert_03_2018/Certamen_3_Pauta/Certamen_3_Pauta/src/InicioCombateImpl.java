30de3ca47136f1d8bc435ec835171896dbbe9073
Eduardo Godoy <eduardo.gl@gmail.com>
2018-08-29 17:57:15 -0300

    clases estructura de datos

--- [INC202]_Programacion_II_2018/certamenes/Pauta_Cert_03_2018/Certamen_3_Pauta/Certamen_3_Pauta/src/InicioCombateImpl.java
+++ [INC202]_Programacion_II_2018/certamenes/Pauta_Cert_03_2018/Certamen_3_Pauta/Certamen_3_Pauta/src/InicioCombateImpl.java
@@ -1,8 +1,8 @@
 public class InicioCombateImpl{
 
     public static void main(String[] args) {
-    	Personaje mago = new Mago(1000);
-    	Personaje guerrero = new Guerrero(1000);
+    	Personaje mago = new Mago(50);
+    	Personaje guerrero = new Guerrero(50);
         Combate combate = new Combate();
     	
         ThreadMago te = new ThreadMago(mago,guerrero,combate);
